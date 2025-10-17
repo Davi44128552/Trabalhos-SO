f8bf6fbaa687633477722c25b9dd8a6ec35693dc
Davi Iury Lopes Souza <554054@UCS.DC.UFC.BR>
2025-10-17 12:17:49 -0300

    Desenvolvendo o algoritmo de round-robin, sem prioridade

--- Trabalho2/Processo.py
+++ Trabalho2/Processo.py
@@ -1,5 +1,6 @@
 # Importando bibliotecas necessárias
 import json
+import copy
 
 # Criando as variáveis globais que serão usadas 
 quantum: int = 0
@@ -83,7 +84,7 @@ def lerProcessos(arquivo: str = "processos.txt"):
 
 # Funções auxiliares
 # Função para verificar se o processo está pronto
-def processos_prontos(timer: int, listaProcessos: list[int]):
+def processos_prontos(timer: int, listaProcessos: list[int]) -> list[int]:
 
     # Pegando os processos prontos para executar
     prontos = []
@@ -96,7 +97,7 @@ def processos_prontos(timer: int, listaProcessos: list[int]):
 
 # Funções para os algoritmos de escalonamento de tarefas
 # First Come First Served
-def FCFS():
+def FCFS() -> list[int]:
 
     ordem_execucao = []
     processosCopia = processos.copy()
@@ -129,7 +130,7 @@ def FCFS():
     return ordem_execucao
 
 # Shortest Job First
-def SJF():
+def SJF() -> list[int]:
 
     ordem_execucao = []
     processosCopia = processos.copy()
@@ -162,7 +163,7 @@ def SJF():
     return ordem_execucao
 
 # Escalonamento por prioridade sem preempção
-def PrioC():
+def PrioC() -> list[int]:
     ordem_execucao = []
     processosCopia = processos.copy()
 
@@ -193,8 +194,71 @@ def PrioC():
     timer = 0
     return ordem_execucao
 
+# Round-robin com quantum, sem prioridade
+def RRSP() -> list[int]:
+    
+    fila_prontos = [] # Pegando uma fila de prontos para não se confundir com os disponíveis
+    processosAdicionados = set()
+    ordem_execucao = []
+    processosCopia = copy.deepcopy(processos) # Criando uma cópia profunda para não modificar os objetos de fato
+
+    global timer # Timer da execução
+
+    # Percorrendo a lista de processos
+    while (processosCopia):
+
+        # Pegando os processos prontos para executar
+        prontos = processos_prontos(timer, processosCopia)
+
+        # Filtrando as tarefas
+        for p in prontos:
+            if (p.tempoRestanteExec > 0 and p.pid not in processosAdicionados):
+                fila_prontos.append(p)
+                processosAdicionados.add(p.pid)
+
+        # Verificando se não há nenhum processo pronto
+        if (not fila_prontos):
+            timer += 1
+            continue
+
+        # Pegando a próxima tarefa da fila a executar
+        prox = fila_prontos.pop(0)
+
+        # Executando a tarefa
+        tempoExecucao = min(quantum, prox.tempoRestanteExec)
+        timer += tempoExecucao
+        prox.tempoRestanteExec -= tempoExecucao
+        ordem_execucao.append(prox.pid)
+
+        # Verificando se surgiu alguma tarefa durante a execução da tarefa
+        prontos = processos_prontos(timer, processosCopia)
+        for p in prontos:
+            if (p.tempoRestanteExec > 0 and p.pid not in processosAdicionados):
+                fila_prontos.append(p)
+                processosAdicionados.add(p.pid)
+
+        # Verificando se a tarefa ainda não terminou
+        if (prox.tempoRestanteExec > 0):
+            fila_prontos.append(prox) # Caso não tenha terminado, vai pro final da fila
+
+        else:
+            processosCopia.remove(prox) # Caso tenha terminado, o removemos da lista de tarefas pendentes
+            processosAdicionados.discard(prox.pid)
+
+
+    # Retornando a lista final
+    timer = 0
+    return ordem_execucao
+
+definirPropriedades()
 lerProcessos()
 lista1 = SJF()
 lista2 = FCFS()
 lista3 = PrioC()
-print(f'Lista de execução por SJF: {lista1} \nLista de execução por FCFS: {lista2} \nLista de execução por PrioC: {lista3}')
+lista4 = RRSP()
+print(f'''
+      Lista de execução por FCFS: {lista2} \n
+      Lista de execução por  SJF: {lista1} \n
+      Lista de execução por PrioC: {lista3} \n
+      Lista de execução por Round-Robin: {lista4} \n'''
+     )
